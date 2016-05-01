70b6368c0cb8bce22378add8ccd4136f91d40589
IAmEmperor <1041618236@qq.com>
2016-05-01 23:55:59 +0800

    First Code

---
+++ OSMS_Emperor1.0/OSMS_Emperor/OSMS_Emperor/OSMS.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace OSMS_Emperor
+{
+    partial class OSMS
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OSMS));
+            this.skinEngine = new Sunisoft.IrisSkin.SkinEngine();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.显示主菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.注销ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsTray.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // skinEngine
+            // 
+            this.skinEngine.@__DrawButtonFocusRectangle = true;
+            this.skinEngine.DisabledButtonTextColor = System.Drawing.Color.Gray;
+            this.skinEngine.DisabledMenuFontColor = System.Drawing.SystemColors.GrayText;
+            this.skinEngine.InactiveCaptionColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.skinEngine.SerialNumber = "";
+            this.skinEngine.SkinFile = null;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.cmsTray;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDown);
+            // 
+            // cmsTray
+            // 
+            this.cmsTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示主菜单ToolStripMenuItem,
+            this.注销ToolStripMenuItem,
+            this.关闭ToolStripMenuItem});
+            this.cmsTray.Name = "contextMenuStrip";
+            this.cmsTray.Size = new System.Drawing.Size(153, 92);
+            // 
+            // 显示主菜单ToolStripMenuItem
+            // 
+            this.显示主菜单ToolStripMenuItem.Name = "显示主菜单ToolStripMenuItem";
+            this.显示主菜单ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.显示主菜单ToolStripMenuItem.Text = "显示主菜单";
+            this.显示主菜单ToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItemShow_Click);
+            // 
+            // 注销ToolStripMenuItem
+            // 
+            this.注销ToolStripMenuItem.Name = "注销ToolStripMenuItem";
+            this.注销ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.注销ToolStripMenuItem.Text = "注销";
+            // 
+            // 关闭ToolStripMenuItem
+            // 
+            this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+            this.关闭ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.关闭ToolStripMenuItem.Text = "关闭";
+            this.关闭ToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItemClose_Click);
+            // 
+            // OSMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OSMS";
+            this.Text = "办公用品管理系统";
+            this.MinimumSizeChanged += new System.EventHandler(this.Form1_MinimumSizeChanged);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.cmsTray.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunisoft.IrisSkin.SkinEngine skinEngine;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip cmsTray;
+        private System.Windows.Forms.ToolStripMenuItem 显示主菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 注销ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
+    }
+}
+
---
+++ OSMS_Emperor1.0/OSMS_Emperor/OSMS_Emperor/OSMS.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IrisSkins;
+
+namespace OSMS_Emperor
+{
+    public partial class OSMS : Form
+    {
+        public OSMS()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //notifyIcon.Visible = false;
+
+            #region 皮肤设置
+            //sr.InitSkins(skinEngine);//初始化皮肤
+            //this.ForeColor = Color.FromArgb(0,0,0); 
+            #endregion
+
+            InitFormControls();//初始化窗体控件
+
+
+        }
+
+        #region 对象实例化
+
+        SkinsRepertories sr = new SkinsRepertories();//皮肤库
+        ComboBox cmbSkins = new ComboBox();//皮肤下拉框 
+
+        #endregion
+
+        #region 全局变量
+
+        private int width;//窗体宽度
+        private int height; //窗体高度
+
+        #endregion
+
+        #region 初始化窗体
+        /// <summary>
+        /// 初始化窗体
+        /// </summary>
+        /// <returns></returns>
+        private bool InitFormControls()
+        {
+            //InitSkinList();//皮肤选择下拉框
+            InitFormSizeAndLoc();//初始化窗体大小和位置
+            InitFormPanel();
+            return true;
+        }
+        #endregion
+
+        #region 初始化窗体大小和位置
+        /// <summary>
+        /// 初始化窗体大小和位置
+        /// </summary>
+        /// <returns></returns>
+        public bool InitFormSizeAndLoc()
+        {
+            width = Screen.PrimaryScreen.Bounds.Width / 4 * 3;
+            height = Screen.PrimaryScreen.Bounds.Height / 4 * 3;
+            this.Width = width;
+            this.Height = height;
+            this.Location = new Point(width / 6, height / 6);
+            return true;
+        } 
+        #endregion
+
+        #region 初始化换肤下拉框
+        /// <summary>
+        /// 初始化换肤下拉框
+        /// </summary>
+        /// <returns></returns>
+        private bool InitSkinList()
+        {
+            BindingSource bs = new BindingSource();
+            bs.DataSource = sr.skins;
+            cmbSkins.DataSource = bs;
+            cmbSkins.DisplayMember = "key";
+            cmbSkins.ValueMember = "Value";
+            cmbSkins.Size = new System.Drawing.Size(80, 10);
+            cmbSkins.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbSkins.Location = new Point(this.Width - 110, 10);
+            cmbSkins.SelectedIndexChanged += new EventHandler(cmbSkinsSelChange);
+            this.Controls.Add(cmbSkins);
+            return true;
+        }
+
+        /// <summary>
+        /// 下拉框选择事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void cmbSkinsSelChange(object sender, EventArgs e)
+        {
+            ComboBox cmb = sender as ComboBox;
+            //MessageBox.Show(cmb.SelectedValue.ToString());
+            sr.ChangeSkins(skinEngine, cmb.SelectedValue.ToString());
+        }
+        #endregion
+
+        #region 初始化窗体布局
+        /// <summary>
+        /// 初始化窗体布局
+        /// </summary>
+        /// <returns></returns>
+        public bool InitFormPanel()
+        {
+            return true;
+        } 
+        #endregion
+
+        #region 界面最大化事件
+        /// <summary>
+        /// 界面最大化事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            cmbSkins.Location = new Point(this.Width - 110, 10);
+        }
+        #endregion
+
+        #region 最小化事件
+        /// <summary>
+        /// 最小化事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MinimumSizeChanged(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 关闭窗体时间
+        /// <summary>
+        /// 关闭窗体时间
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!notifyIcon.Visible)
+            {
+                if (MessageBox.Show("是否最小化到托盘", "提示", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    this.Hide();
+                    notifyIcon.Visible = true;
+                    // notifyIcon.ShowBalloonTip(5000, "Hello", "I'm Emperor", ToolTipIcon.Info);
+                    e.Cancel = true;
+                }
+            }
+        }
+        #endregion
+
+        #region 托盘点击事件
+        /// <summary>
+        /// 托盘点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void notifyIcon_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Show();
+                notifyIcon.Visible = false;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                cmsTray.Show();
+            }
+        }
+        #endregion
+
+        #region 托盘右击菜单点击事件
+        /// <summary>
+        /// 托盘右击菜单点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItemShow_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            notifyIcon.Visible = false;
+        }
+
+        private void ToolStripMenuItemClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+    }
+}
